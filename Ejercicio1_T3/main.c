d154d849691fd40e455fd96fe50743d49a83ac4e
jrg00094 <jrg00094@red.ujaen.es>
2026-02-24 19:17:04 +0100

    Tema 3 Ejercicio 1

---
+++ Ejercicio1_T3/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int x = 5;
+    int *p = &x;
+
+    // 1. Imprimir x, &x, p, *p
+    printf("Valor de x: %d\n", x);          // El valor actual de x
+    printf("Dirección de x (&x): %p\n", (void*)&x); // Dónde está x en la memoria
+    printf("Valor de p: %p\n", (void*)p);   // La dirección de p en la memoria (que es &x)
+    printf("Valor de *p: %d\n", *p);        // Qué hay en la dirección p
+
+    // 2. Modificar x a 99 usando p
+    *p = 99; // Cambiar *p, cambia x
+
+    // 3. Imprimir x
+    printf("Nuevo valor de x: %d\n", x);
+
+    return 0;
+}
