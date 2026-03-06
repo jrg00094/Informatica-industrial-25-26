6270fa95adf5920fd6a5f6ce9431c18057319ec9
jrg00094 <jrg00094@red.ujaen.es>
2026-03-06 09:02:17 +0100

    Parte A Tema 4

--- Tema_4/Parte_A/include/funciones.h
+++ Tema_4/Parte_A/include/funciones.h
@@ -1,6 +1,13 @@
 #include <stdlib.h>
-
-
+#ifndef FUNCIONES_H
+/**
+ * Leemos un entero de entrada por teclado
+ *      pront: cadena de texto a mostrar.
+ *      out: entero introducido por teclado.
+ *      return: 1 lectura correcta, 0 error de lectura.
+ */
 int leer_entero(const char *prompt, int *out);
 int leer_float(const char *prompt, float *out);
 int leer_cadena(const char *prompt, char *dst, size_t cap);
+
+#endif
